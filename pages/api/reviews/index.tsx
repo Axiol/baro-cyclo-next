59472cededb756cffc5c8f4c0e69d3ef1c78adfd
Arnaud Delante <contact@arnaud.ninja>
2023-03-11 00:53:10 +0100

    Create the create review call

--- pages/api/reviews/index.tsx
+++ pages/api/reviews/index.tsx
@@ -1,15 +1,39 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-
-import clientPromise from '@libs/mongodb'
+import { PrismaClient } from '@prisma/client'
 
 const places = async (req: NextApiRequest, res: NextApiResponse) => {
-  const client = await clientPromise
-  const db = client.db('baro-cyclo')
+  const prisma = new PrismaClient()
 
   switch (req.method) {
     case 'POST':
-      const places = await // res.json({ status: 200, data: places })
-      db.collection('reviews').insertOne({})
+      const body = req.body
+      if (!body.placeId || !body.score) {
+        res.status(400).json({
+          code: 'MISSING-BODY',
+          message: 'Missing parameters',
+        })
+
+        return
+      }
+
+      const review = await prisma.review
+        .create({
+          data: {
+            placeId: body.placeId,
+            score: JSON.stringify(body.score),
+          },
+        })
+        .catch(async (e) => {
+          console.error(e)
+          res.status(500).json({
+            code: 'INTERNAL-ERROR',
+            message: 'Something went wrong when saving the data',
+          })
+          return
+        })
+
+      res.json({ status: 200, data: review })
+      break
   }
 }
 
